699379d77522f2eb854f388af43fe82e9ade2253
yashweblife <yashweblife20@gmail.com>
2024-08-16 00:48:25 -0500

    initial entry

--- web/AwesomHA/src/App.tsx
+++ web/AwesomHA/src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import { Box, HStack, IconButton, VStack } from "@chakra-ui/react"
+import { BsFillDeviceSsdFill } from 'react-icons/bs'
+import { FaCog, FaRegUser, FaRobot } from 'react-icons/fa'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Box width="100vw" height="100vh" backgroundColor={'rgb(32,32,32)'}>
+      <HStack width="100vw" height="100vh">
+        <VStack backgroundColor={'rgb(12,12,12)'} height={'100%'} p={2} spacing={10}>
+          <IconButton aria-label="" color='white' fontSize={'3xl'} isRound variant='none' icon={<FaRegUser />}>1</IconButton>
+          <IconButton aria-label="" color='white' fontSize={'3xl'} isRound variant='none' icon={<BsFillDeviceSsdFill />}>1</IconButton>
+          <IconButton aria-label="" color='white' fontSize={'3xl'} isRound variant='none' icon={<FaRobot />}>2</IconButton>
+          <IconButton aria-label="" color='white' fontSize={'3xl'} isRound variant='none' icon={<FaCog />}>1</IconButton>
+        </VStack>
+
+      </HStack>
+    </Box>
   )
 }
 
