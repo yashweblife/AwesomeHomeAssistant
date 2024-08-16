da617e2fd56f873714fc44503c33961db43b0028
yashweblife <yashweblife20@gmail.com>
2024-08-16 17:13:18 -0500

    add dark mode default

--- web/AwesomHA/src/main.tsx
+++ web/AwesomHA/src/main.tsx
@@ -1,10 +1,15 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ChakraProvider>
+  <ChakraProvider theme={extendTheme(ThemeConfig)}>
     <App />
   </ChakraProvider>
 )
