645b2c8edd3bf2c9915f853f974c5cbeabdbbd5b
yashweblife <yashweblife20@gmail.com>
2024-08-16 17:13:00 -0500

    implement router

--- web/AwesomHA/src/App.tsx
+++ web/AwesomHA/src/App.tsx
@@ -1,6 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Auth from './pages/Auth/Auth'
+import Dashboard from './pages/Dashboard/Dashboard'
+
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Auth />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
