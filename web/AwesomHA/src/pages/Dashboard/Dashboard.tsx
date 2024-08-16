c83f8e09a642abb54630c16960fe104261d32ff4
yashweblife <yashweblife20@gmail.com>
2024-08-16 01:25:57 -0500

    add basic dashboard

--- web/AwesomHA/src/pages/Dashboard/Dashboard.tsx
+++ web/AwesomHA/src/pages/Dashboard/Dashboard.tsx
@@ -1,3 +1,17 @@
+import { Box, HStack, IconButton, VStack } from "@chakra-ui/react";
+import { BsFillDeviceSsdFill } from 'react-icons/bs';
+import { FaCog, FaRegUser, FaRobot } from 'react-icons/fa';
 export default function Dashboard() {
-    return <></>
+    return (
+        <Box width="100vw" height="100vh" backgroundColor={'rgb(32,32,32)'}>
+        <HStack width="100vw" height="100vh">
+          <VStack backgroundColor={'rgb(12,12,12)'} height={'100%'} p={2} spacing={10}>
+            <IconButton aria-label="" color='white' fontSize={'3xl'} isRound variant='none' icon={<FaRegUser />}>1</IconButton>
+            <IconButton aria-label="" color='white' fontSize={'3xl'} isRound variant='none' icon={<BsFillDeviceSsdFill />}>1</IconButton>
+            <IconButton aria-label="" color='white' fontSize={'3xl'} isRound variant='none' icon={<FaRobot />}>2</IconButton>
+            <IconButton aria-label="" color='white' fontSize={'3xl'} isRound variant='none' icon={<FaCog />}>1</IconButton>
+          </VStack>
+        </HStack>
+      </Box>
+    )
 }
