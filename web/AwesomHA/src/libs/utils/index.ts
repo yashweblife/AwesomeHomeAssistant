dc30c2ffdd07eccf6598a79342c05b6ac26fe72e
yashweblife <yashweblife20@gmail.com>
2024-08-18 19:22:51 -0500

    add util types

---
+++ web/AwesomHA/src/libs/utils/index.ts
@@ -0,0 +1,31 @@
+export type Command = {
+    name: string,
+    info: string,
+    command: string
+}
+
+export type Device = {
+    name: string
+    id: string
+    status: string
+    type: string
+    room: string
+    url: string
+    commands:Command[]
+}
+
+export type User = {
+    name: string
+    email: string
+    devices:Device
+    id: string
+}
+
+export type Todo = {
+    name: string
+    description: string
+    id: string
+    user: User
+    status: string
+    date: string
+}
