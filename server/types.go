1f98df330ca1ef71b5c2f745c0f1d071f34028b5
yashweblife <yashweblife20@gmail.com>
2024-08-22 16:24:53 -0500

    add new dbms and types

---
+++ server/types.go
@@ -0,0 +1,21 @@
+package main
+
+type Command struct {
+	Name string `json:"name"`
+	Info string `json:"info"`
+}
+
+type Device struct {
+	ID       string    `json:"id"`
+	IP       string    `json:"ip"`
+	Type     bool      `json:"type"`
+	Name     string    `json:"name"`
+	Commands []Command `json:"commands"`
+}
+type User struct {
+	ID       string   `json:"id"`
+	Name     string   `json:"name"`
+	Email    string   `json:"email"`
+	Password string   `json:"password"`
+	Devices  []Device `json:"devices"`
+}
