ee2c81879eae91c0c395d731376938a7239ede4f
yashweblife <yashweblife20@gmail.com>
2024-08-24 17:54:01 -0500

    create modal

---
+++ web/AwesomHA/src/components/AddDeviceModal/index.tsx
@@ -0,0 +1,48 @@
+import { Button, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/react";
+import { useState } from "react";
+
+type AddDeviceModalProps = {
+    isOpen: boolean
+    onClose: () => void
+}
+export default function AddDeviceModal({ isOpen, onClose }: AddDeviceModalProps) {
+    const [modalDeviceName, setModalDeviceName] = useState('')
+    const [modalDeviceUrl, setModalDeviceUrl] = useState('http://192.168.')
+
+    const handleAddButton = async () => {
+        const output = {
+            name: modalDeviceName,
+            url: modalDeviceUrl
+        }
+        try {
+            const test = await fetch("http://localhost:8080/device/register", {
+                method: "POST",
+                body: JSON.stringify(output),
+            })
+            if (!test.ok) {
+                throw new Error("error")
+            }
+            onClose()
+        } catch (error) {
+        }
+    }
+    return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Add Device</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <Input placeholder="Device Name" value={modalDeviceName} onChange={(e) => setModalDeviceName(e.target.value)}></Input>
+                    <Input placeholder="Device URL" value={modalDeviceUrl} onChange={(e) => setModalDeviceUrl(e.target.value)}></Input>
+                </ModalBody>
+                <ModalFooter>
+                    <Button colorScheme='blue' mr={3} onClick={onClose}>
+                        Close
+                    </Button>
+                    <Button variant='ghost' onClick={handleAddButton}>Add</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
+}
