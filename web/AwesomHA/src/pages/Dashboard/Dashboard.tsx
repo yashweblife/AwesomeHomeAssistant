78081de4db550bf1a8b5d661de34272f5f98da36
yashweblife <yashweblife20@gmail.com>
2024-08-26 21:39:33 -0500

    import graph view

--- web/AwesomHA/src/pages/Dashboard/Dashboard.tsx
+++ web/AwesomHA/src/pages/Dashboard/Dashboard.tsx
@@ -1,15 +1,19 @@
 import { Box, HStack, useDisclosure, VStack } from "@chakra-ui/react";
 import AddDeviceModal from "../../components/AddDeviceModal";
+import Chart from "../../components/Chart";
 import DashboardSidebar from "../../components/DashboardSidebar";
 import { SensorDisplay } from "../../components/SensorDisplay";
 export default function Dashboard() {
-  const {isOpen, onOpen, onClose} =  useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Box width="100vw" height="100vh" backgroundColor={'rgb(32,32,32)'}>
       <HStack width="100vw" height="100vh">
-        <DashboardSidebar onOpen={onOpen}/>
+        <DashboardSidebar sidebarOpen={onOpen} />
         <VStack height={'100%'} p={2}>
           <SensorDisplay name="light" url="http://localhost:8080/"></SensorDisplay>
+        </VStack>
+        <VStack>
+          <Chart />
         </VStack>
       </HStack>
       <AddDeviceModal isOpen={isOpen} onClose={onClose} />
