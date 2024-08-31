bbe4139ec1201be5ece8bcee52b41b39822050a4
yashweblife <yashweblife20@gmail.com>
2024-08-31 02:56:16 -0500

    add db as var

--- server/DBMS/dbms.go
+++ server/DBMS/dbms.go
@@ -4,26 +4,21 @@ import (
 	"database/sql"
 )
 
-type DBMS struct {
-	DB *sql.DB
-}
+func Initialize() error {
 
-func (dbms *DBMS) Initialize() error {
-
-	var err error
-	dbms.DB, err = sql.Open("sqlite3", "./AwesomeHA.db")
+	DB, err := sql.Open("sqlite3", "./AwesomeHA.db")
 	if err != nil {
 		return err
 	}
-	_, err = dbms.DB.Exec("CREATE TABLE IF NOT EXISTS USERS (id TEXT, name TEXT, email TEXT, password TEXT)")
+	_, err = DB.Exec("CREATE TABLE IF NOT EXISTS USERS (id TEXT, name TEXT, email TEXT, password TEXT)")
 	if err != nil {
 		return err
 	}
-	_, err = dbms.DB.Exec("CREATE TABLE IF NOT EXISTS DEVICES (id TEXT, url TEXT, name TEXT, owner TEXT)")
+	_, err = DB.Exec("CREATE TABLE IF NOT EXISTS DEVICES (id TEXT, url TEXT, name TEXT, owner TEXT)")
 	if err != nil {
 		return err
 	}
-	_, err = dbms.DB.Exec("CREATE TABLE IF NOT EXISTS GROUPS (id TEXT, name TEXT, owner TEXT, members TABLE)")
+	_, err = DB.Exec("CREATE TABLE IF NOT EXISTS GROUPS (id TEXT, name TEXT, owner TEXT, members TABLE)")
 	if err != nil {
 		return err
 	}
