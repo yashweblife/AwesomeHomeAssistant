6f1b35a0a6d74e4f4a03c3ea292c1332f7edfa50
yashweblife <yashweblife20@gmail.com>
2024-09-02 21:16:16 -0500

    remove errors from userManager, gin doesnt like it

--- server/userManager.go
+++ server/userManager.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,7 +9,7 @@ import (
 	"github.com/google/uuid"
 )
 
-func LoginUser(c *gin.Context) error {
+func LoginUser(c *gin.Context) {
 	type AuthLoginType struct {
 		Email    string `json:"email"`
 		Password string `json:"password"`
@@ -19,77 +18,64 @@ func LoginUser(c *gin.Context) error {
 	if err := c.Bind(&auth); err != nil {
 		log.Println("Error binding request body: ", err)
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	if auth.Email == "" || auth.Password == "" {
 		log.Println("Error: Email and password are required")
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Email and password are required"})
-		return errors.New("Email and password are required")
 	}
 	log.Println("Received request for login: ", auth)
 	err := AuthenticateLoginAttempt(auth.Email, auth.Password)
 	if err != nil {
 		log.Println("Error: Authentication returned null")
 		c.JSON(http.StatusInternalServerError, gin.H{"data": "Authentication returned null"})
-		return errors.New("Authentication returned null")
 	}
 	log.Println("Response from authentication: ", err)
 	c.JSON(http.StatusOK, gin.H{"data": "Success"})
-	return nil
 }
-func AddUser(c *gin.Context) error {
+func AddUser(c *gin.Context) {
 	var user User
 	userID := uuid.New().String()
 	fmt.Println("User ID: ", userID)
 	if err := c.Bind(&user); err != nil {
 		fmt.Println(err)
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	var didCreateUser bool
 	err := AddUserToDB(userID, user.Name, user.Email, user.Password, &didCreateUser)
 	if err != nil {
 		fmt.Println(err)
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	fmt.Println("User created: ", didCreateUser)
 	if !didCreateUser {
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return errors.New("Bad Request")
 	}
 	c.JSON(http.StatusOK, gin.H{"data": userID})
-	return nil
 }
-func GetUsers(c *gin.Context) error {
+func GetUsers(c *gin.Context) {
 	var users []User
 	err := GetAllUsers(users)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	c.JSON(200, gin.H{"data": users})
-	return nil
 }
-func GetUser(c *gin.Context) error {
+func GetUser(c *gin.Context) {
 	type input struct {
 		Id string `json:"id"`
 	}
 	var id input
 	if err := c.ShouldBindJSON(&id); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	var userEmail string
 	err := GetUserInfo(id.Id, &userEmail)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	c.JSON(200, gin.H{"data": userEmail})
-	return nil
 }
-func EditUserInfo(c *gin.Context) error {
+func EditUserInfo(c *gin.Context) {
 
 	type input struct {
 		Id       string `json:"id"`
@@ -100,12 +86,10 @@ func EditUserInfo(c *gin.Context) error {
 	var id input
 	if err := c.ShouldBindJSON(&id); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	c.JSON(200, gin.H{"data": "Edited"})
-	return nil
 }
-func RemoveUser(c *gin.Context) error {
+func RemoveUser(c *gin.Context) {
 
 	type input struct {
 		Id string `json:"id"`
@@ -113,14 +97,11 @@ func RemoveUser(c *gin.Context) error {
 	var id input
 	if err := c.ShouldBindJSON(&id); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	var didRemoveUser bool
 	err := RemoveUserFromDB(id.Id, &didRemoveUser)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	c.JSON(200, gin.H{"data": didRemoveUser})
-	return nil
 }
