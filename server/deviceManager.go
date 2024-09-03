c35f585490e3c75728360b6fd86e4d8087137acc
yashweblife <yashweblife20@gmail.com>
2024-09-02 21:11:58 -0500

    remove returns, gin doesnt like them

--- server/deviceManager.go
+++ server/deviceManager.go
@@ -13,87 +13,71 @@ import (
 type DeviceManager struct {
 }
 
-func (dm *DeviceManager) RegisterDevice(c *gin.Context) error {
+func (dm *DeviceManager) RegisterDevice(c *gin.Context) {
 	var device Device
 	if err := c.ShouldBindJSON(&device); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	err := AddDeviceToDB(uuid.New().String(), device.URL, device.Name)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	c.JSON(200, gin.H{"data": "Registered"})
-	return nil
 }
-func (dm *DeviceManager) DeleteDevice(c *gin.Context) error {
+func (dm *DeviceManager) DeleteDevice(c *gin.Context) {
 	var id string
 	if err := c.ShouldBindJSON(&id); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	err := RemoveDeviceFromDB(id)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	c.JSON(200, gin.H{"data": "Removed"})
-	return nil
 }
-func (dm *DeviceManager) UpdateDevice(c *gin.Context) error {
-	return nil
+func (dm *DeviceManager) UpdateDevice(c *gin.Context) {
 }
-func (dm *DeviceManager) GetDevice(c *gin.Context) error {
+func (dm *DeviceManager) GetDevice(c *gin.Context) {
 	var id string
 	if err := c.ShouldBindJSON(&id); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	var device Device
 	err := GetDeviceFromDB(id, &device)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	c.JSON(200, gin.H{"data": device})
-	return nil
 }
-func (dm *DeviceManager) GetDevices(c *gin.Context) error {
+func (dm *DeviceManager) GetDevices(c *gin.Context) {
 	var devices []Device
 	err := GetAllDevices(&devices)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"data": "Bad Request"})
-		return err
 	}
 	c.JSON(200, gin.H{"data": devices})
-	return nil
 }
-func (dm *DeviceManager) SendRequestToDevice(c *gin.Context) error {
+func (dm *DeviceManager) SendRequestToDevice(c *gin.Context) {
 	type outputData struct {
 		Value int `json:"value"`
 	}
 	res, err := http.Get("http://192.168.0.29:81/value")
 	if err != nil {
 		c.JSON(http.StatusNoContent, gin.H{"data": "No Host"})
-		return err
 	}
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		log.Fatal(err)
 		c.JSON(http.StatusNoContent, gin.H{"data": "No Host"})
-		return err
 	}
 	var data outputData
 	err = json.Unmarshal(body, &data)
 	if err != nil {
 		log.Fatal(err)
 		c.JSON(http.StatusNoContent, gin.H{"data": "No Host"})
-		return err
 	}
 	c.JSON(http.StatusOK, data)
-	return nil
 }
 
 func (dm *DeviceManager) SendDoesWorkMessage(c *gin.Context) {
