3cd51718872b851fc01f2cbc100d5850ddf1d107
yashweblife <yashweblife20@gmail.com>
2024-09-02 21:12:38 -0500

    add deviceManager to main code

--- server/main.go
+++ server/main.go
@@ -10,7 +10,7 @@ const PORT = ":8080"
 func main() {
 	r := gin.Default()
 	r.Use(cors.Default())
-
+	dm := DeviceManager{}
 	InitDatabase()
 	auth_route := r.Group("/auth")
 	{
@@ -22,12 +22,12 @@ func main() {
 	}
 	device_route := r.Group("/device")
 	{
-		device_route.GET("/", SendDoesWorkMessage)
-		device_route.GET("value", SendRequestToDevice)
-		device_route.POST("register", RegisterDevice)
-		device_route.DELETE("delete", DeleteDevice)
-		device_route.GET("devices", GetDevices)
-		device_route.GET("device", GetDevice)
+		device_route.GET("/", dm.SendDoesWorkMessage)
+		device_route.GET("value", dm.SendRequestToDevice)
+		device_route.POST("register", dm.RegisterDevice)
+		device_route.DELETE("delete", dm.DeleteDevice)
+		device_route.GET("devices", dm.GetDevices)
+		device_route.GET("device", dm.GetDevice)
 	}
 	r.Run(PORT)
 }
