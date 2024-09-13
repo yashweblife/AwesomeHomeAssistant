cb425ddb23fee9c94d88ca6230d105a191755e41
yashweblife <yashweblife20@gmail.com>
2024-09-13 16:32:43 -0500

    fixes

--- server/dbms/dbms.go
+++ server/dbms/dbms.go
@@ -1,13 +1,28 @@
 package dbms
 
 import (
-	"fmt"
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func CreateBasicCheck() {
-	fmt.Println("::Connecting to database...")
+type DBMS struct {
 }
 
-func CreateAdvanceCheck() {
-	fmt.Println("::Connecting to advanced database...")
+var DB *sql.DB
+
+func (d *DBMS) Init() error {
+	DB, err := sql.Open("sqlite3", "./AwesomeHA.db")
+	if err != nil {
+		return err
+	}
+	_, err = DB.Exec("CREATE TABLE IF NOT EXISTS USERS (id TEXT, name TEXT, email TEXT, password TEXT)")
+	if err != nil {
+		return err
+	}
+	_, err = DB.Exec("CREATE TABLE IF NOT EXISTS DEVICES (id TEXT, url TEXT, name TEXT)")
+	if err != nil {
+		return err
+	}
+	return nil
 }
