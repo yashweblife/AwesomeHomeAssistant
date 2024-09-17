57c58f897ffbbc4349c8223a876c3ab685a7f3bf
yashweblife <yashweblife20@gmail.com>
2024-09-17 14:54:06 -0500

    add more functions

--- server/iot/iot.go
+++ server/iot/iot.go
@@ -6,3 +6,12 @@ type IOT struct {
 func (iot *IOT) Init() error {
 	return nil
 }
+func (iot *IOT) CheckIfOnline() (bool, error) {
+	return true, nil
+}
+func (iot *IOT) GetCommands() (string, error) {
+	return "{}", nil
+}
+func (iot *IOT) CallCommand(name string) (string, error) {
+	return "{}", nil
+}
