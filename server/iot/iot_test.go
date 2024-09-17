ecfc8ac9d6207648f63980151a445de161b8a8d6
yashweblife <yashweblife20@gmail.com>
2024-09-16 19:45:19 -0700

    add test for init iot

--- server/iot/iot_test.go
+++ server/iot/iot_test.go
@@ -3,3 +3,14 @@ package iot
 import (
 	"testing"
 )
+
+func TestIOT(t *testing.T) {
+	iot := IOT{}
+	t.Run("Init", func(t *testing.T) {
+		err := iot.Init()
+		if err != nil {
+			t.Fail()
+		}
+	})
+
+}
