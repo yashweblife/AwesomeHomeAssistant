f558ef4287e6bb4966698e6797c44e20c7806f9f
yashweblife <yashweblife20@gmail.com>
2024-09-17 15:00:59 -0500

    add more tests for iot

--- server/iot/iot_test.go
+++ server/iot/iot_test.go
@@ -13,4 +13,32 @@ func TestIOT(t *testing.T) {
 		}
 	})
 
+	t.Run("Check If Online", func(t *testing.T) {
+		online, error := iot.CheckIfOnline()
+		if error != nil {
+			t.Fail()
+		}
+		if online {
+			t.Log("Working")
+		}
+	})
+	t.Run("Get Commands", func(t *testing.T) {
+		commands, error := iot.GetCommands()
+		if error != nil {
+			t.Fail()
+		}
+		if commands == "{}" {
+			t.Log("Working")
+		}
+	})
+	t.Run("Call Command", func(t *testing.T) {
+		command, error := iot.CallCommand("test")
+		if error != nil {
+			t.Fail()
+		}
+		if command == "{}" {
+			t.Log("Working")
+		}
+	})
+
 }
