142be4806139c00120a3dc4a68e9b5b52341d361
yashweblife <yashweblife20@gmail.com>
2024-09-16 19:26:19 -0700

    add test 1

--- server/dbms/dbms_test.go
+++ server/dbms/dbms_test.go
@@ -4,3 +4,14 @@ import (
 	"testing"
 )
 
+func TestDBMS(t *testing.T){
+	dbms := DBMS{}
+	t.Run("Init", func (t *testing.T) {
+		err := dbms.Init()
+		if err != nil {
+			t.Errorf("Init() error = %v", err)
+		}
+
+		t.Log("Init() ok")
+	})
+}
