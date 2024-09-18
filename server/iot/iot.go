6ea0a2ea652f4a4775d4efbbe69dec77ee279b87
yashweblife <yashweblife20@gmail.com>
2024-09-18 11:44:15 -0500

    implement init function

--- server/iot/iot.go
+++ server/iot/iot.go
@@ -1,9 +1,30 @@
 package iot
 
+import (
+	"errors"
+	"net/http"
+)
+
 type IOT struct {
+	url string
 }
 
 func (iot *IOT) Init() error {
+	if iot.url == "" {
+		iot.url = "http://192.168.0.20:81/"
+	}
+	client := http.DefaultClient
+	req, err := http.NewRequest("GET", iot.url, nil)
+	if err != nil {
+		return err
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != 200 {
+		return errors.New("Failed to connect to IOT")
+	}
 	return nil
 }
 func (iot *IOT) CheckIfOnline() (bool, error) {
