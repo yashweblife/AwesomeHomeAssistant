14eba37290cb59625c20c9ae7dfcc2035dd1f563
yashweblife <yashweblife20@gmail.com>
2024-09-18 11:51:11 -0500

    implement get commands

--- server/iot/iot.go
+++ server/iot/iot.go
@@ -45,8 +45,35 @@ func (iot *IOT) CheckIfOnline() (bool, error) {
 	}
 	return true, nil
 }
-func (iot *IOT) GetCommands() (string, error) {
-	return "{}", nil
+
+type DeviceCommand struct {
+	Name string `json:"name"`
+	info string `json:"info"`
+}
+type DeviceInfo struct {
+	Type     string          `json:"type"`
+	Info     string          `json:"info"`
+	IP       string          `json:"ip"`
+	Commands []DeviceCommand `json:"commands"`
+}
+
+func (iot *IOT) GetCommands() ([]DeviceCommand, error) {
+	if iot.url == "" {
+		iot.url = "http://192.168.0.20:81/"
+	}
+	client := http.DefaultClient
+	req, err := http.NewRequest("GET", iot.url, nil)
+	if err != nil {
+		return []DeviceCommand{}, err
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return []DeviceCommand{}, err
+	}
+	if res.StatusCode != 200 {
+		return []DeviceCommand{}, errors.New("Failed to connect to IOT")
+	}
+	return []DeviceCommand{}, nil
 }
 func (iot *IOT) CallCommand(name string) (string, error) {
 	return "{}", nil
