53c6dddfaa38a3923d0e6da6c16747c0111cfe6c
yashweblife <yashweblife20@gmail.com>
2024-09-18 14:08:18 -0500

    trials

--- server/dbms/dbms_test.go
+++ server/dbms/dbms_test.go
@@ -6,20 +6,32 @@ import (
 
 func TestDBMS(t *testing.T) {
 	dbms := DBMS{}
+	var user User
 	t.Run("Init", func(t *testing.T) {
 		err := dbms.Init()
 		if err != nil {
 			t.Fail()
 		}
 	})
-	t.Run("AddUserToDB", func(t *testing.T) {
-		id, err := dbms.AddUserToDB("test", "test", "test")
+
+	t.Run("Add User", func(t *testing.T) {
+		userID, err := dbms.AddUser("test", "test", "test")
+		if err != nil {
+			t.Log(err)
+			t.Fail()
+		}
+		user.ID = userID
+		user.Name = "test"
+		user.Email = "test"
+		user.Password = "test"
+		user.Devices = "{}"
+		t.Log("Added User", user)
+	})
+	t.Run("Get User", func(t *testing.T) {
+		user, err := dbms.GetUser(user.ID)
 		if err != nil {
 			t.Fail()
 		}
-		if id == "" {
-			t.Fail()
-		}
-		t.Log(id)
+		t.Log("Got User", user)
 	})
 }
