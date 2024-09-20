5ce2e8dd5b60175ebb0c29fc41a6bf8b2cb5aab6
yashweblife <yashweblife20@gmail.com>
2024-09-20 15:58:59 -0500

    working

--- server/dbms/dbms.go
+++ server/dbms/dbms.go
@@ -2,27 +2,27 @@ package dbms
 
 import (
 	"database/sql"
-	"fmt"
+	"errors"
 
 	"github.com/google/uuid"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type DBMS struct {
+	DB *sql.DB
 }
 
-var DB *sql.DB
-
 func (d *DBMS) Init() error {
-	DB, err := sql.Open("sqlite3", "./AwesomeHA.db")
+	var err error
+	d.DB, err = sql.Open("sqlite3", "./AwesomeHA.db")
 	if err != nil {
 		return err
 	}
-	_, err = DB.Exec("CREATE TABLE IF NOT EXISTS USERS (id TEXT, name TEXT, email TEXT, password TEXT, devices TEXT)")
+	_, err = d.DB.Exec("CREATE TABLE IF NOT EXISTS USERS (id TEXT, name TEXT, email TEXT, password TEXT, devices TEXT)")
 	if err != nil {
 		return err
 	}
-	_, err = DB.Exec("CREATE TABLE IF NOT EXISTS DEVICES (id TEXT, url TEXT, name TEXT, commands TEXT)")
+	_, err = d.DB.Exec("CREATE TABLE IF NOT EXISTS DEVICES (id TEXT, url TEXT, name TEXT, commands TEXT)")
 	if err != nil {
 		return err
 	}
@@ -32,17 +32,14 @@ func (d *DBMS) Init() error {
 func (d *DBMS) AddUser(name, email, password string) (string, error) {
 	userID := uuid.New().String()
 	var count int
-	fmt.Println(&email)
-	err := DB.QueryRow("SELECT COUNT(*) FROM USERS WHERE email = ?", email).Scan(&count)
-
+	err := d.DB.QueryRow("SELECT COUNT(*) FROM USERS WHERE email = ?", email).Scan(&count)
 	if err != nil {
 		return "", err
 	}
-	fmt.Println("INSIDE ADD USER TO DB", userID, email)
 	if count > 0 {
-		return "", nil
+		return "", errors.New("User already exists")
 	}
-	_, err = DB.Exec("INSERT INTO USERS (id, name, email, password) VALUES (?,?,?,?)", userID, name, email, password)
+	_, err = d.DB.Exec("INSERT INTO USERS (id, name, email, password, devices) VALUES (?,?,?,?,?)", userID, name, email, password, "{}")
 	if err != nil {
 		return "", err
 	}
@@ -51,7 +48,7 @@ func (d *DBMS) AddUser(name, email, password string) (string, error) {
 
 func (d *DBMS) GetUser(userID string) (User, error) {
 	var user User
-	row := DB.QueryRow("SELECT * FROM USERS WHERE id = ?", userID)
+	row := d.DB.QueryRow("SELECT * FROM USERS WHERE id = ?", userID)
 	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.Password, &user.Devices)
 	if err != nil {
 		return user, err
